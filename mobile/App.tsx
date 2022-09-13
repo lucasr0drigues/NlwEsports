99ecdafc60cef0be05487a52962a3259a1a1cc25
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-09-13 05:54:51 -0300

    add web assets

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>{props.title}</Text>
+    </TouchableOpacity>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello World!</Text>
+
+      <Button title="Send 1" />
+      <Button title="Send 2" />
+      <Button title="Send 3" />
+      <Button title="Hello World!" />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,5 @@
+import "./styles.css";
+
 function App() {
   return <h1>Hello NLW</h1>;
 }
