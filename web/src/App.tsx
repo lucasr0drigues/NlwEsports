fcf0086ab362e6901ce201470a0668ea8a4450b7
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-09-14 23:36:18 -0300

    add mobile assets

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,7 +6,7 @@ import logoImg from "./assets/logo-nlw-esports.png";
 
 function App() {
   return (
-    <div className="max-w-[1344] mx-auto flex flex-col items-center my-20">
+    <div className="max-w-max mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
 
       <h1 className="text-6xl text-white font-black mt-20">
@@ -67,13 +67,13 @@ function App() {
         </a>
       </div>
 
-      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8">
+      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
         <div className="bg-[#2a2643] px-8 py-6 flex justify-between items-center">
           <div>
             <strong className="text-2xl text-white font-black block">
               Não encontrou seu duo?
             </strong>
-            <span className="text-zinc-400">
+            <span className="text-zinc-400 block">
               Publique um anúncio para encontrar novos players!
             </span>
           </div>
