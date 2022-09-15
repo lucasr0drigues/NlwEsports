21658a4676ad23f28a568e5fe9ba4103bd1c3309
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-09-14 23:02:07 -0300

    add publicar anuncio box

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,12 +2,15 @@
 module.exports = {
   content: ["./src/**/*.tsx", "./index.html"],
   theme: {
+    fontFamily: { sans: ["Inter", "sans-serif"] },
     extend: {
       colors: {},
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
         "nlw-gradient":
           "linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 40.94%, #E1D55D 33.57%)",
+        "game-gradient":
+          "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)",
       },
     },
   },
