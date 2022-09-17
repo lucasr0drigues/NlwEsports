2991fbbb2d187c9c6f90c2936ddc4e2771af5e93
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-09-17 18:02:09 -0300

    add prisma client

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.get("/games", (request, response) => {
+  const games = prisma.game.findMany({});
+
   return response.json([]);
 });
 
